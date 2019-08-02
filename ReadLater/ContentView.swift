43188f0899c78a8477faf47302f19806f6a23a61
Jakob Sen <j.ereksen@gmail.com>
2019-08-02 20:12:42 +0200

    Initial Commit

---
+++ ReadLater/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ReadLater
+//
+//  Created by Jakob Sen on 02.08.19.
+//  Copyright © 2019 Jakob Sen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
